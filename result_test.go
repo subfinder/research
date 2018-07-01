95587505c481278c9f0a47e0012e3b038f48e73c
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-01 11:33:27 -0400

    add BenchmarkInitializeEmptyPointerToResultMulitThreaded() function

--- result_test.go
+++ result_test.go
@@ -650,3 +650,15 @@ func BenchmarkInitializeSuccessResultMulitThreaded(b *testing.B) {
 	}
 	wg.Wait()
 }
+
+func BenchmarkInitializeEmptyPointerToResultMulitThreaded(b *testing.B) {
+	wg := sync.WaitGroup{}
+	for n := 0; n < b.N; n++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_ = &Result{}
+		}()
+	}
+	wg.Wait()
+}
