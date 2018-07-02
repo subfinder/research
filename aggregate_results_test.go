65c7eae79b1f468196c0aa0d8ced1f12239985b1
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-02 15:19:29 -0400

    add TestAggregateCustomResults() function

--- aggregate_results_test.go
+++ aggregate_results_test.go
@@ -61,3 +61,35 @@ func TestAggregateFailedResults(t *testing.T) {
 	}
 }
 
+func TestAggregateCustomResults(t *testing.T) {
+	fakeResults := []*Result{
+		&Result{Success: true},
+		&Result{Success: false},
+		&Result{Success: 0},
+		&Result{Success: "wiggle"},
+		&Result{Failure: errors.New("example1")},
+		&Result{Failure: errors.New("example2")},
+	}
+
+	fakeResultsChan := make(chan *Result)
+	go func(fakeResults []*Result, fakeResultsChan chan *Result) {
+		defer close(fakeResultsChan)
+		for _, result := range fakeResults {
+			fakeResultsChan <- result
+		}
+	}(fakeResults, fakeResultsChan)
+
+	counter := 0
+
+	for _ = range AggregateCustomResults(fakeResultsChan, func(r *Result) bool {
+		_, ok := r.Success.(bool)
+		return ok
+	}) {
+		counter++
+	}
+
+	if counter != 2 {
+		t.Fatalf("expected '%v' successful results, got '%v'", 2, counter)
+	}
+}
+
