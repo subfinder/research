fd45fc33b9272819e1aa01b1e771b4dcaa0ab639
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-02 15:19:09 -0400

    add TestAggregateFailedResults() function

--- aggregate_results_test.go
+++ aggregate_results_test.go
@@ -33,3 +33,31 @@ func TestAggregateSuccessfulResults(t *testing.T) {
 	}
 }
 
+func TestAggregateFailedResults(t *testing.T) {
+	fakeResults := []*Result{
+		&Result{Success: true},
+		&Result{Success: 0},
+		&Result{Success: "wiggle"},
+		&Result{Failure: errors.New("example1")},
+		&Result{Failure: errors.New("example2")},
+	}
+
+	fakeResultsChan := make(chan *Result)
+	go func(fakeResults []*Result, fakeResultsChan chan *Result) {
+		defer close(fakeResultsChan)
+		for _, result := range fakeResults {
+			fakeResultsChan <- result
+		}
+	}(fakeResults, fakeResultsChan)
+
+	counter := 0
+
+	for _ = range AggregateFailuedResults(fakeResultsChan) {
+		counter++
+	}
+
+	if counter != 2 {
+		t.Fatalf("expected '%v' failed results, got '%v'", 2, counter)
+	}
+}
+
