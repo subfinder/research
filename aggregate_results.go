ac6f286ad01cfcc1861c0b6a5a97679a02275baf
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-02 15:17:58 -0400

    add AggregateFailuedResults() function

--- aggregate_results.go
+++ aggregate_results.go
@@ -13,3 +13,16 @@ func AggregateSuccessfulResults(in chan *Result) <-chan *Result {
 	return out
 }
 
+func AggregateFailuedResults(in chan *Result) <-chan *Result {
+	out := make(chan *Result)
+	go func(in, out chan *Result) {
+		defer close(out)
+		for result := range in {
+			if result.Failure != nil {
+				out <- result
+			}
+		}
+	}(in, out)
+	return out
+}
+
