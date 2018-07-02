a3af83ad2121f7a74589cb9c180aadbf1646adbb
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-02 19:06:15 -0400

    add ExampleAggregateSuccessfulResults function

--- aggregate_results_test.go
+++ aggregate_results_test.go
@@ -159,3 +159,31 @@ func ExampleAggregateCustomResults() {
 	// green
 	// blue
 }
+
+func ExampleAggregateSuccessfulResults() {
+	fakeResults := []*Result{
+		&Result{Success: true},
+		&Result{Success: 0},
+		&Result{Success: "wiggle"},
+		&Result{Failure: errors.New("example1")},
+		&Result{Failure: errors.New("example2")},
+	}
+
+	fakeResultsChan := make(chan *Result)
+	go func(fakeResults []*Result, fakeResultsChan chan *Result) {
+		defer close(fakeResultsChan)
+		for _, result := range fakeResults {
+			fakeResultsChan <- result
+		}
+	}(fakeResults, fakeResultsChan)
+
+	counter := 0
+
+	for _ = range AggregateSuccessfulResults(fakeResultsChan) {
+		counter++
+	}
+
+	fmt.Println(counter)
+	// Output: 3
+}
+
