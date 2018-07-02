fece583376fd59f74025ffef6ba7568d5d33edf9
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-02 15:17:48 -0400

    add AggregateSuccessfulResults() function

--- aggregate_results.go
+++ aggregate_results.go
@@ -1,2 +1,15 @@
 package subzero
 
+func AggregateSuccessfulResults(in chan *Result) <-chan *Result {
+	out := make(chan *Result)
+	go func(in, out chan *Result) {
+		defer close(out)
+		for result := range in {
+			if result.Failure == nil {
+				out <- result
+			}
+		}
+	}(in, out)
+	return out
+}
+
