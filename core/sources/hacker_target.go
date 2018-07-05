bf83e5112c82a12a4a487d0dcb7ed5d3072c4e86
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-05 12:52:25 -0400

    add required ProcessDomain() method to satisfy the core Source interface
    this implementation can be updated in the future as it has some hardcoded values at the moment

--- core/sources/hacker_target.go
+++ core/sources/hacker_target.go
@@ -9,3 +9,35 @@ import "strings"
 
 type HackerTarget struct{}
 
+func (source *HackerTarget) ProcessDomain(domain string) <-chan *core.Result {
+	results := make(chan *core.Result)
+	go func(domain string, results chan *core.Result) {
+		defer close(results)
+
+		httpClient := &http.Client{
+			Timeout: time.Second * 4,
+			Transport: &http.Transport{
+				Dial: (&net.Dialer{
+					Timeout: 5 * time.Second,
+				}).Dial,
+				TLSHandshakeTimeout: 5 * time.Second,
+			},
+		}
+
+		// get response from the API
+		resp, err := httpClient.Get("https://api.hackertarget.com/hostsearch/?q=" + domain)
+		if err != nil {
+			results <- &core.Result{Type: "hacker target", Failure: err}
+			return
+		}
+		defer resp.Body.Close()
+
+		// TODO: investigate io.LimitedReader
+		// read response body, extracting subdomains
+		scanner := bufio.NewScanner(resp.Body)
+		for scanner.Scan() {
+			results <- &core.Result{Type: "hacker target", Success: strings.Split(scanner.Text(), ",")[0]}
+		}
+	}(domain, results)
+	return results
+}
