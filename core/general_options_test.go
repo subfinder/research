1baa757885e3d6c365f35dd67524555177c74930
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-05 10:40:12 -0400

    add TestGeneralOptions() function

--- core/general_options_test.go
+++ core/general_options_test.go
@@ -5,3 +5,28 @@ import "reflect"
 import "time"
 import "runtime"
 
+func TestGeneralOptions(t *testing.T) {
+	opts := &GeneralOptions{}
+	var units = []struct {
+		got interface{}
+		exp interface{}
+	}{
+		{opts.Verbose, false},
+		{opts.ColorSupport, false},
+		{opts.AvailableCores, 0},
+		{opts.Recursive, false},
+		{opts.PassiveOnly, false},
+		{opts.IgnoreErrors, false},
+		{opts.OutputType, ""},
+		{opts.OutputDir, ""},
+		{len(opts.TargetDomains), 0},
+		{len(opts.Sources), 0},
+		{len(opts.Resolvers), 0},
+	}
+	for _, u := range units {
+		if !reflect.DeepEqual(u.exp, u.got) {
+			t.Fatalf("expected '%v', got '%v'", u.exp, u.got)
+		}
+	}
+}
+
