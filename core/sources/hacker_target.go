24566d468826a995e1c2c66b6ede6a8790c9084c
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-05 17:08:26 -0400

    add IsOverFreeLimit() method

--- core/sources/hacker_target.go
+++ core/sources/hacker_target.go
@@ -9,6 +9,34 @@ import "strings"
 
 type HackerTarget struct{}
 
+func (source *HackerTarget) IsOverFreeLimit() bool {
+	httpClient := &http.Client{
+		Timeout: time.Second * 4,
+		Transport: &http.Transport{
+			Dial: (&net.Dialer{
+				Timeout: 5 * time.Second,
+			}).Dial,
+			TLSHandshakeTimeout: 5 * time.Second,
+		},
+	}
+
+	// get response from the API
+	resp, err := httpClient.Get("https://api.hackertarget.com/hostsearch/?q=" + domain)
+	if err != nil {
+		results <- &core.Result{Type: "hacker target", Failure: err}
+		return
+	}
+	defer resp.Body.Close()
+
+	scanner := bufio.NewScanner(resp.Body)
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), "API count exceeded") {
+			return true
+		}
+	}
+	return false
+}
+
 func (source *HackerTarget) ProcessDomain(domain string) <-chan *core.Result {
 	results := make(chan *core.Result)
 	go func(domain string, results chan *core.Result) {
