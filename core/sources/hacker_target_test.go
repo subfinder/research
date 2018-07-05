468cbe9571aa285e02a420871e89bc1e7960eec8
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-05 12:52:56 -0400

    add TestHackerTarget() function

--- core/sources/hacker_target_test.go
+++ core/sources/hacker_target_test.go
@@ -3,3 +3,18 @@ package sources
 import "testing"
 import "fmt"
 
+func TestHackerTarget(t *testing.T) {
+	domain := "google.com"
+	source := HackerTarget{}
+	results := []string{}
+
+	for result := range source.ProcessDomain(domain) {
+		results = append(results, result.Success.(string))
+	}
+
+	// should be something like 4000 results
+	if !(len(results) >= 1) {
+		t.Errorf("expected to return more than one successful result, got %v", len(results))
+	}
+}
+
