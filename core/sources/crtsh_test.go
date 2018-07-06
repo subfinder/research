0c29b219d448b6d9cf8e94569978a354b25926f1
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-06 11:35:01 -0400

    add TestCrtSh() function

--- core/sources/crtsh_test.go
+++ core/sources/crtsh_test.go
@@ -5,3 +5,17 @@ import "testing"
 import "sync"
 import "fmt"
 
+func TestCrtSh(t *testing.T) {
+	domain := "bing.com"
+	source := CrtSh{}
+	results := []*core.Result{}
+
+	for result := range source.ProcessDomain(domain) {
+		results = append(results, result)
+	}
+
+	if !(len(results) >= 500) {
+		t.Errorf("expected more than 500 result(s), got '%v'", len(results))
+	}
+}
+
