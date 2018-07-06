a0c1160c3e9312c8beeb3b5a1fb527325f56ddf5
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-06 11:35:24 -0400

    add ExampleCrtSh_MultiThreaded() function

--- core/sources/crtsh_test.go
+++ core/sources/crtsh_test.go
@@ -59,3 +59,29 @@ func ExampleCrtSh() {
 	// Output: true
 }
 
+func ExampleCrtSh_MultiThreaded() {
+	domains := []string{"google.com", "bing.com", "yahoo.com", "duckduckgo.com"}
+	source := CrtSh{}
+	results := []*core.Result{}
+
+	wg := sync.WaitGroup{}
+	mx := sync.Mutex{}
+
+	for _, domain := range domains {
+		wg.Add(1)
+		go func(domain string) {
+			defer wg.Done()
+			for result := range source.ProcessDomain(domain) {
+				mx.Lock()
+				results = append(results, result)
+				mx.Unlock()
+			}
+		}(domain)
+	}
+
+	wg.Wait() // collect results
+
+	fmt.Println(len(results) > 1180)
+	// Output: true
+}
+
