71d51e3c7dbab13ded089019c9e54a7eecd38e00
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-06 17:20:16 -0400

    add start of riddler.go

---
+++ core/sources/riddler.go
@@ -0,0 +1,12 @@
+package sources
+
+import core "github.com/subfinder/research/core"
+import "net/http"
+import "strings"
+import "encoding/json"
+import "bytes"
+import "bufio"
+import "net"
+import "time"
+import "errors"
+
