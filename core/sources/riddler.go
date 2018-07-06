2f87b786ef4edbf28780f144d352e48168243025
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-06 17:24:17 -0400

    add Authenticate() method
    This method is rather lenght, admitteldy.
    
    * it makes a POST request to the authorization endpoint for riddler using email/password
    * from there it decodes the json response and checks the `authentication_token`.
    * if there's a token, the source's token will be set
    
    Note: this is probably not that thread safe and could be revised to be safer?

--- core/sources/riddler.go
+++ core/sources/riddler.go
@@ -28,3 +28,47 @@ type riddlerAuthenticationResponse struct {
 	} `json:"response"`
 }
 
+// Note: probably not totally thread safe? #yolo
+func (source *Riddler) Authenticate() (bool, error) {
+	httpClient := &http.Client{
+		Timeout: time.Second * 60,
+		Transport: &http.Transport{
+			Dial: (&net.Dialer{
+				Timeout: 5 * time.Second,
+			}).Dial,
+			TLSHandshakeTimeout: 5 * time.Second,
+		},
+	}
+
+	var data = []byte(`{"email":"` + source.Email + `", "password":"` + source.Password + `"}`)
+
+	// Create a post request to get subdomain data
+	req, err := http.NewRequest("POST", "https://riddler.io/auth/login", bytes.NewBuffer(data))
+	if err != nil {
+		return false, err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	auth := &riddlerAuthenticationResponse{}
+
+	err = json.NewDecoder(resp.Body).Decode(&auth)
+	if err != nil {
+		return false, err
+	}
+
+	if auth.Response.User.AuthenticationToken == "" {
+		return false, errors.New("failed to get authentication token")
+	}
+
+	source.APIToken = auth.Response.User.AuthenticationToken
+
+	return true, nil
+}
+
