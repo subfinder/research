4aa8fb23c16a26c15c0d799419755add90000441
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-05 22:16:48 -0400

    add TestCertSpotter() function

--- core/sources/certspotter_test.go
+++ core/sources/certspotter_test.go
@@ -5,3 +5,20 @@ import "testing"
 import "sync"
 import "fmt"
 
+func TestCertSpotter(t *testing.T) {
+	domain := "google.com"
+	source := CertSpotter{}
+	results := []*core.Result{}
+
+	for result := range source.ProcessDomain(domain) {
+		if result.IsFailure() {
+			t.Fatal(result.Failure)
+		}
+		results = append(results, result)
+	}
+
+	if !(len(results) >= 5000) {
+		t.Errorf("expected more than 5000 results, got '%v'", len(results))
+	}
+}
+
