0d28cf25eed574ba545b87b84d87b89deb8108cc
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-06 14:57:38 -0400

    add TestThreatminer() function

--- core/sources/threatminer_test.go
+++ core/sources/threatminer_test.go
@@ -5,3 +5,17 @@ import "testing"
 import "sync"
 import "fmt"
 
+func TestThreatminer(t *testing.T) {
+	domain := "bing.com"
+	source := Threatminer{}
+	results := []*core.Result{}
+
+	for result := range source.ProcessDomain(domain) {
+		results = append(results, result)
+	}
+
+	if !(len(results) >= 140) {
+		t.Errorf("expected more than 140 result(s), got '%v'", len(results))
+	}
+}
+
