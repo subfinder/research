db062422111c0d6790a0b663328e1a72eea5355a
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-06 17:25:29 -0400

    add TestRiddler() function

--- core/sources/riddler_test.go
+++ core/sources/riddler_test.go
@@ -5,3 +5,17 @@ import "testing"
 import "sync"
 import "fmt"
 
+func TestRiddler(t *testing.T) {
+	domain := "bing.com"
+	source := Riddler{}
+	results := []*core.Result{}
+
+	for result := range source.ProcessDomain(domain) {
+		results = append(results, result)
+	}
+
+	if !(len(results) >= 9) {
+		t.Errorf("expected more than 9 result(s), got '%v'", len(results))
+	}
+}
+
