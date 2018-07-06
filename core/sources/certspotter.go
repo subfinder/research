fba96c2707a503c7a72962d417719fe975d875f2
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-05 22:16:16 -0400

    add ProcessDomain() method
    to satisfy the core.Source interface

--- core/sources/certspotter.go
+++ core/sources/certspotter.go
@@ -13,3 +13,44 @@ type certspotterObject struct {
 	DNSNames []string `json:"dns_names"`
 }
 
+func (source *CertSpotter) ProcessDomain(domain string) <-chan *core.Result {
+	results := make(chan *core.Result)
+	go func(domain string, results chan *core.Result) {
+		defer close(results)
+
+		httpClient := &http.Client{
+			Timeout: time.Second * 60,
+			Transport: &http.Transport{
+				Dial: (&net.Dialer{
+					Timeout: 5 * time.Second,
+				}).Dial,
+				TLSHandshakeTimeout: 5 * time.Second,
+			},
+		}
+
+		// get response from the API, optionally with an API key
+		resp, err := httpClient.Get("https://certspotter.com/api/v0/certs?domain=" + domain)
+		if err != nil {
+			results <- &core.Result{Type: "certspotter", Failure: err}
+			return
+		}
+		defer resp.Body.Close()
+
+		certspotterData := []*certspotterObject{}
+		err = json.NewDecoder(resp.Body).Decode(&certspotterData)
+		if err != nil {
+			results <- &core.Result{Type: "certspotter", Failure: err}
+			return
+		}
+		for _, block := range certspotterData {
+			for _, dnsName := range block.DNSNames {
+				// Fix Wildcard subdomains containing asterisk before them
+				if strings.Contains(dnsName, "*.") {
+					dnsName = strings.Split(dnsName, "*.")[1]
+				}
+				results <- &core.Result{Type: "certspotter", Success: dnsName}
+			}
+		}
+	}(domain, results)
+	return results
+}
