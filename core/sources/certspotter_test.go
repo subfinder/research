b963092252e83cf70cdc633a24158eedbf2abd11
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-05 22:17:36 -0400

    add ExampleCertSpotter_MultiThreaded() function

--- core/sources/certspotter_test.go
+++ core/sources/certspotter_test.go
@@ -62,3 +62,29 @@ func ExampleCertSpotter() {
 	// Output: true
 }
 
+func ExampleCertSpotter_MultiThreaded() {
+	domains := []string{"google.com", "bing.com", "yahoo.com", "duckduckgo.com"}
+	source := CertSpotter{}
+	results := []*core.Result{}
+
+	wg := sync.WaitGroup{}
+	mx := sync.Mutex{}
+
+	for _, domain := range domains {
+		wg.Add(1)
+		go func(domain string) {
+			defer wg.Done()
+			for result := range source.ProcessDomain(domain) {
+				mx.Lock()
+				results = append(results, result)
+				mx.Unlock()
+			}
+		}(domain)
+	}
+
+	wg.Wait() // collect results
+
+	fmt.Println(len(results) > 67000)
+	// Output: true
+}
+
