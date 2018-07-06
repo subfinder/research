706dab55438ba0ee893b1693528bba095f9d4147
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-06 14:57:48 -0400

    add TestThreatminerMultiThreaded() function

--- core/sources/threatminer_test.go
+++ core/sources/threatminer_test.go
@@ -19,3 +19,30 @@ func TestThreatminer(t *testing.T) {
 	}
 }
 
+func TestThreatminerMultiThreaded(t *testing.T) {
+	domains := []string{"google.com", "bing.com", "yahoo.com", "duckduckgo.com"}
+	source := Threatminer{}
+	results := []*core.Result{}
+
+	wg := sync.WaitGroup{}
+	mx := sync.Mutex{}
+
+	for _, domain := range domains {
+		wg.Add(1)
+		go func(domain string) {
+			defer wg.Done()
+			for result := range source.ProcessDomain(domain) {
+				mx.Lock()
+				results = append(results, result)
+				mx.Unlock()
+			}
+		}(domain)
+	}
+
+	wg.Wait() // collect results
+
+	if len(results) < 3500 {
+		t.Errorf("expected more than 1180 results, got '%v'", len(results))
+	}
+}
+
