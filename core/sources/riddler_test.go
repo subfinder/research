42fbbc0921dd419c4a03f53b3658df7d3b3c52f4
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-06 17:26:02 -0400

    add ExampleRiddlerMultiThreaded() function

--- core/sources/riddler_test.go
+++ core/sources/riddler_test.go
@@ -59,3 +59,29 @@ func ExampleRiddler() {
 	// Output: true
 }
 
+func ExampleRiddlerMultiThreaded() {
+	domains := []string{"google.com", "bing.com", "yahoo.com", "duckduckgo.com"}
+	source := Riddler{}
+	results := []*core.Result{}
+
+	wg := sync.WaitGroup{}
+	mx := sync.Mutex{}
+
+	for _, domain := range domains {
+		wg.Add(1)
+		go func(domain string) {
+			defer wg.Done()
+			for result := range source.ProcessDomain(domain) {
+				mx.Lock()
+				results = append(results, result)
+				mx.Unlock()
+			}
+		}(domain)
+	}
+
+	wg.Wait() // collect results
+
+	fmt.Println(len(results) >= 30)
+	// Output: true
+}
+
