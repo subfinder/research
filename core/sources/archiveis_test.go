261ff69f853d639d24980ab7517810a76b94ae9d
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-08 15:19:30 -0400

    add TestArchiveIs() function

--- core/sources/archiveis_test.go
+++ core/sources/archiveis_test.go
@@ -5,3 +5,18 @@ import "testing"
 import "sync"
 import "fmt"
 
+func TestArchiveIs(t *testing.T) {
+	domain := "bing.com"
+	source := ArchiveIs{}
+	results := []*core.Result{}
+
+	for result := range source.ProcessDomain(domain) {
+		results = append(results, result)
+	}
+
+	//t.Log(len(results), "\n", "Success: ", success, "Failure: ", failure)
+	if !(len(results) >= 20) {
+		t.Errorf("expected more than 20 result(s), got '%v'", len(results))
+	}
+}
+
