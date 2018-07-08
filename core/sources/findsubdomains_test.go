34a85e7ddd035c8d9ec4b2cabaa172b29fd603a7
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-08 16:31:58 -0400

    add TestFindSubdomainsDotCom() function

--- core/sources/findsubdomains_test.go
+++ core/sources/findsubdomains_test.go
@@ -5,3 +5,17 @@ import "testing"
 import "sync"
 import "fmt"
 
+func TestFindSubdomainsDotCom(t *testing.T) {
+	domain := "bing.com"
+	source := FindSubdomainsDotCom{}
+	results := []*core.Result{}
+
+	for result := range source.ProcessDomain(domain) {
+		results = append(results, result)
+	}
+
+	if !(len(results) >= 400) {
+		t.Errorf("expected more than 400 result(s), got '%v'", len(results))
+	}
+}
+
