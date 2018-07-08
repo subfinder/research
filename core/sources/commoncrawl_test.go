dc15421476be2ef081bd618c5ed054974d4ae29f
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-08 17:03:43 -0400

    add TestCommonCrawlDotOrg() function

--- core/sources/commoncrawl_test.go
+++ core/sources/commoncrawl_test.go
@@ -5,3 +5,17 @@ import "testing"
 import "sync"
 import "fmt"
 
+func TestCommonCrawlDotOrg(t *testing.T) {
+	domain := "bing.com"
+	source := CommonCrawlDotOrg{}
+	results := []*core.Result{}
+
+	for result := range source.ProcessDomain(domain) {
+		results = append(results, result)
+	}
+
+	if !(len(results) >= 3) {
+		t.Errorf("expected at least 3 result(s), got '%v'", len(results))
+	}
+}
+
