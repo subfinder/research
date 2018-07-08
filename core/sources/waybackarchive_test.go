d8c465594bb33f7b8fbc33fbaa893491bfdbd771
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-08 14:40:37 -0400

    add TestWaybackArchiveMultiThreaded() function

--- core/sources/waybackarchive_test.go
+++ core/sources/waybackarchive_test.go
@@ -28,3 +28,30 @@ func TestWaybackArchive(t *testing.T) {
 	}
 }
 
+func TestWaybackArchiveMultiThreaded(t *testing.T) {
+	domains := []string{"google.com", "bing.com", "yahoo.com", "duckduckgo.com"}
+	source := WaybackArchive{}
+	results := []*core.Result{}
+
+	wg := sync.WaitGroup{}
+	mx := sync.Mutex{}
+
+	for _, domain := range domains {
+		wg.Add(1)
+		go func(domain string) {
+			defer wg.Done()
+			for result := range source.ProcessDomain(domain) {
+				mx.Lock()
+				results = append(results, result)
+				mx.Unlock()
+			}
+		}(domain)
+	}
+
+	wg.Wait() // collect results
+
+	if len(results) <= 4 {
+		t.Errorf("expected at least 4 results, got '%v'", len(results))
+	}
+}
+
