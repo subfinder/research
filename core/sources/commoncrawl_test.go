1dfdc9d43c3d2fef056246ab579ae591487178ce
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-08 17:03:53 -0400

    add TestCommonCrawlDotOrgMultiThreaded() function

--- core/sources/commoncrawl_test.go
+++ core/sources/commoncrawl_test.go
@@ -19,3 +19,30 @@ func TestCommonCrawlDotOrg(t *testing.T) {
 	}
 }
 
+func TestCommonCrawlDotOrgMultiThreaded(t *testing.T) {
+	domains := []string{"google.com", "bing.com", "yahoo.com", "duckduckgo.com"}
+	source := CommonCrawlDotOrg{}
+	results := []*core.Result{}
+
+	wg := sync.WaitGroup{}
+	mx := sync.Mutex{}
+
+	for _, domain := range domains {
+		wg.Add(1)
+		go func(domain string) {
+			defer wg.Done()
+			for result := range source.ProcessDomain(domain) {
+				mx.Lock()
+				results = append(results, result)
+				mx.Unlock()
+			}
+		}(domain)
+	}
+
+	wg.Wait() // collect results
+
+	if len(results) < 40 {
+		t.Errorf("expected more than 40 results, got '%v'", len(results))
+	}
+}
+
