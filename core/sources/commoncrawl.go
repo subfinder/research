cc67bdd3632af5b707901095ee1c32f287b34d0b
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-08 17:03:23 -0400

    add ProcessDomain() method

--- core/sources/commoncrawl.go
+++ core/sources/commoncrawl.go
@@ -9,3 +9,48 @@ import "regexp"
 
 type CommonCrawlDotOrg struct{}
 
+func (source *CommonCrawlDotOrg) ProcessDomain(domain string) <-chan *core.Result {
+	results := make(chan *core.Result)
+	go func(domain string, results chan *core.Result) {
+		defer close(results)
+
+		httpClient := &http.Client{
+			Timeout: time.Second * 60,
+			Transport: &http.Transport{
+				Dial: (&net.Dialer{
+					Timeout: 5 * time.Second,
+				}).Dial,
+				TLSHandshakeTimeout: 5 * time.Second,
+			},
+		}
+
+		resp, err := httpClient.Get("http://index.commoncrawl.org/CC-MAIN-2018-17-index?url=*." + domain + "&output=json")
+		if err != nil {
+			results <- &core.Result{Type: "commoncrawldotorg", Failure: err}
+			return
+		}
+		defer resp.Body.Close()
+
+		domainExtractor, err := regexp.Compile(`((\w|_|-|\*)+\.)+` + domain)
+		if err != nil {
+			results <- &core.Result{Type: "commoncrawldotorg", Failure: err}
+			return
+		}
+
+		uniqFilter := map[string]bool{}
+
+		scanner := bufio.NewScanner(resp.Body)
+
+		for scanner.Scan() {
+			for _, str := range domainExtractor.FindAllString(scanner.Text(), -1) {
+				_, found := uniqFilter[str]
+				if !found {
+					uniqFilter[str] = true
+					results <- &core.Result{Type: "commoncrawldotorg", Success: str}
+				}
+			}
+		}
+
+	}(domain, results)
+	return results
+}
