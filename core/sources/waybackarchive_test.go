5582e9886396e9c79264889f491862282fefef07
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-08 14:40:23 -0400

    add TestWaybackArchive() function

--- core/sources/waybackarchive_test.go
+++ core/sources/waybackarchive_test.go
@@ -5,3 +5,26 @@ import "testing"
 import "sync"
 import "fmt"
 
+func TestWaybackArchive(t *testing.T) {
+	domain := "bing.com"
+	source := WaybackArchive{}
+	results := []*core.Result{}
+
+	success := 0
+	failure := 0
+
+	for result := range source.ProcessDomain(domain) {
+		results = append(results, result)
+		if result.Failure != nil {
+			failure += 1
+		} else {
+			success += 1
+		}
+	}
+
+	t.Log(len(results), "\n", "Success: ", success, "Failure: ", failure)
+	if !(len(results) >= 1) {
+		t.Errorf("expected more than 1 result(s), got '%v'", len(results))
+	}
+}
+
