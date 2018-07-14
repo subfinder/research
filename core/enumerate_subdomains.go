d105b456df087bea3e25eabfb47caf338be84b21
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-14 13:38:04 -0400

    simplify EnumerateSubdomains() implementation

--- core/enumerate_subdomains.go
+++ core/enumerate_subdomains.go
@@ -17,20 +17,19 @@ import "sync"
 //
 func EnumerateSubdomains(domain string, options *EnumerationOptions) <-chan *Result {
 	results := make(chan *Result)
-	go func(domain string, options *EnumerationOptions, results chan *Result) {
+	go func() {
 		defer close(results)
 		wg := sync.WaitGroup{}
 		for _, source := range options.Sources {
 			wg.Add(1)
-			// merge source results channels with go funcs
-			go func(source Source, results chan *Result) {
+			go func(source Source) {
 				defer wg.Done()
 				for result := range source.ProcessDomain(domain) {
 					results <- result
 				}
-			}(source, results)
+			}(source)
 		}
 		wg.Wait()
-	}(domain, options, results)
+	}()
 	return results
 }
