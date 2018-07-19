3758b3a698851a4448519c2276b1414218283f8e
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-19 12:44:47 -0400

    add ProcessDomain() method
    to satisfy the core.Source interface

--- core/sources/certdb.go
+++ core/sources/certdb.go
@@ -9,3 +9,47 @@ import "regexp"
 
 type CertDB struct{}
 
+func (source *CertDB) ProcessDomain(domain string) <-chan *core.Result {
+	results := make(chan *core.Result)
+	go func(domain string, results chan *core.Result) {
+		defer close(results)
+
+		httpClient := &http.Client{
+			Timeout: time.Second * 60,
+			Transport: &http.Transport{
+				Dial: (&net.Dialer{
+					Timeout: 5 * time.Second,
+				}).Dial,
+				TLSHandshakeTimeout: 5 * time.Second,
+			},
+		}
+
+		uniqFilter := map[string]bool{}
+
+		domainExtractor, err := regexp.Compile(`[a-zA-Z0-9\*_.-]+\.` + domain)
+		if err != nil {
+			results <- &core.Result{Type: "certdb", Failure: err}
+			return
+		}
+
+		resp, err := httpClient.Get("https://certdb.com/domain/" + domain)
+		if err != nil {
+			results <- &core.Result{Type: "certdb", Failure: err}
+			return
+		}
+		defer resp.Body.Close()
+
+		scanner := bufio.NewScanner(resp.Body)
+
+		for scanner.Scan() {
+			for _, str := range domainExtractor.FindAllString(scanner.Text(), -1) {
+				_, found := uniqFilter[str]
+				if !found {
+					uniqFilter[str] = true
+					results <- &core.Result{Type: "certdb", Success: str}
+				}
+			}
+		}
+	}(domain, results)
+	return results
+}
