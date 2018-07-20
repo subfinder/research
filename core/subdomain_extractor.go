0243eb9aa762d061288a2a58c0a6fe629f0cb768
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-20 11:46:55 -0400

    add NewSubdomainExtractor() function

--- core/subdomain_extractor.go
+++ core/subdomain_extractor.go
@@ -2,3 +2,12 @@ package subzero
 
 import "regexp"
 
+// NewSubdomainExtractor creates a new regular expression to extract
+// subdomains from text based on the given domain.
+func NewSubdomainExtractor(domain string) (*regexp.Regexp, error) {
+	extractor, err := regexp.Compile(`[a-zA-Z0-9\*_.-]+\.` + domain)
+	if err != nil {
+		return nil, err
+	}
+	return extractor, nil
+}
