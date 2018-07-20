9c8e6c70a99df820b5ed3da163611466ba2b5e54
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-20 11:47:36 -0400

    add ExampleNewSubdomainExtractor() function

--- core/subdomain_extractor_test.go
+++ core/subdomain_extractor_test.go
@@ -10,3 +10,16 @@ func TestNewSubdomainExtractor(t *testing.T) {
 	}
 }
 
+func ExampleNewSubdomainExtractor() {
+	exampleText := `<a href="https://subdomain.google.com">`
+
+	extractor, err := NewSubdomainExtractor("google.com")
+	if err != nil {
+		panic(err)
+	}
+
+	subdomain := extractor.FindString(exampleText)
+
+	fmt.Println(subdomain)
+	// Output: subdomain.google.com
+}
