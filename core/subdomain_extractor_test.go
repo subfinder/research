8eba1bd6b1d6c6450ac074e2618d77fb567dfd21
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-07-20 11:47:25 -0400

    add TestNewSubdomainExtractor() function

--- core/subdomain_extractor_test.go
+++ core/subdomain_extractor_test.go
@@ -3,3 +3,10 @@ package subzero
 import "testing"
 import "fmt"
 
+func TestNewSubdomainExtractor(t *testing.T) {
+	_, err := NewSubdomainExtractor("google.com")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
