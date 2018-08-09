8a09b766a41d2e174740cfe70630e39324b84958
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-08-09 13:54:20 -0400

    add ExampleAsk_multi_threaded function

--- core/sources/ask_test.go
+++ core/sources/ask_test.go
@@ -72,3 +72,29 @@ func ExampleAsk() {
 	// Output: true
 }
 
+func ExampleAsk_multi_threaded() {
+	domains := []string{"google.com", "bing.com", "yahoo.com", "duckduckgo.com"}
+	source := Ask{}
+	results := []*core.Result{}
+
+	wg := sync.WaitGroup{}
+	mx := sync.Mutex{}
+
+	for _, domain := range domains {
+		wg.Add(1)
+		go func(domain string) {
+			defer wg.Done()
+			for result := range source.ProcessDomain(domain) {
+				mx.Lock()
+				results = append(results, result)
+				mx.Unlock()
+			}
+		}(domain)
+	}
+
+	wg.Wait() // collect results
+
+	fmt.Println(len(results) >= 4)
+	// Output: true
+}
+
