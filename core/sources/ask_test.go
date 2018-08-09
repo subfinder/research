477d13a50f495f18fd4823d6d2c6d32bc940e76f
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-08-09 13:53:54 -0400

    add TestAsk function

--- core/sources/ask_test.go
+++ core/sources/ask_test.go
@@ -8,3 +8,23 @@ import (
 	"github.com/subfinder/research/core"
 )
 
+func TestAsk(t *testing.T) {
+	domain := "google.com"
+	source := Ask{}
+	results := []*core.Result{}
+
+	for result := range source.ProcessDomain(domain) {
+		t.Log(result)
+		fmt.Println(result)
+		results = append(results, result)
+		// Not waiting around to iterate all the possible pages.
+		if len(results) >= 15 {
+			break
+		}
+	}
+
+	if !(len(results) >= 15) {
+		t.Errorf("expected more than 15 result(s), got '%v'", len(results))
+	}
+}
+
