8558f1db20bf934f6dc9ca476d946b0b9a713cb9
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-08-09 11:26:36 -0400

    add DogPile struct

--- core/sources/dogpile.go
+++ core/sources/dogpile.go
@@ -7,3 +7,11 @@ import (
 	"github.com/subfinder/research/core"
 )
 
+// DogPile is a source to process subdomains from http://dogpile.com
+//
+// Note
+//
+// This source uses http instead of https because of problems dogpile's SSL cert.
+//
+type DogPile struct{}
+
