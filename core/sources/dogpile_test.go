181f490806822243ee97e0ad0dca0101a6af2313
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-08-09 11:28:13 -0400

    add BenchmarkDogPile_multi_threaded function

--- core/sources/dogpile_test.go
+++ core/sources/dogpile_test.go
@@ -109,3 +109,27 @@ func BenchmarkDogPile_single_threaded(b *testing.B) {
 	}
 }
 
+func BenchmarkDogPile_multi_threaded(b *testing.B) {
+	domains := []string{"google.com", "bing.com", "yahoo.com", "duckduckgo.com"}
+	source := DogPile{}
+	wg := sync.WaitGroup{}
+	mx := sync.Mutex{}
+
+	for n := 0; n < b.N; n++ {
+		results := []*core.Result{}
+
+		for _, domain := range domains {
+			wg.Add(1)
+			go func(domain string) {
+				defer wg.Done()
+				for result := range source.ProcessDomain(domain) {
+					mx.Lock()
+					results = append(results, result)
+					mx.Unlock()
+				}
+			}(domain)
+		}
+
+		wg.Wait() // collect results
+	}
+}
