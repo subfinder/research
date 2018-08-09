6871ebfa763a319be5fc07eeed4bbe0e897427d8
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-08-09 11:27:56 -0400

    add ExampleDogPile_multi_threaded function

--- core/sources/dogpile_test.go
+++ core/sources/dogpile_test.go
@@ -71,3 +71,29 @@ func ExampleDogPile() {
 	// Output: true
 }
 
+func ExampleDogPile_multi_threaded() {
+	domains := []string{"google.com", "bing.com", "yahoo.com", "duckduckgo.com"}
+	source := DogPile{}
+	results := []*core.Result{}
+
+	wg := sync.WaitGroup{}
+	mx := sync.Mutex{}
+
+	for _, domain := range domains {
+		wg.Add(1)
+		go func(domain string) {
+			defer wg.Done()
+			for result := range source.ProcessDomain(domain) {
+				mx.Lock()
+				results = append(results, result)
+				mx.Unlock()
+			}
+		}(domain)
+	}
+
+	wg.Wait() // collect results
+
+	fmt.Println(len(results) >= 4)
+	// Output: true
+}
+
