cded8e8e50c45927886687ed2cdb24ce33e9a327
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-08-09 11:27:02 -0400

    add ProcessDomain() method
    to satisfy core.Source interface

--- core/sources/dogpile.go
+++ core/sources/dogpile.go
@@ -15,3 +15,45 @@ import (
 //
 type DogPile struct{}
 
+// ProcessDomain takes a given base domain and attempts to enumerate subdomains.
+func (source *DogPile) ProcessDomain(domain string) <-chan *core.Result {
+	results := make(chan *core.Result)
+	go func(domain string, results chan *core.Result) {
+		defer close(results)
+
+		domainExtractor, err := core.NewSubdomainExtractor(domain)
+		if err != nil {
+			results <- core.NewResult("dogpile", nil, err)
+			return
+		}
+
+		uniqFilter := map[string]bool{}
+
+		for currentPage := 1; currentPage <= 750; currentPage++ {
+			resp, err := core.HTTPClient.Get("http://www.dogpile.com/search/web?q=" + domain + "&qsi=" + strconv.Itoa(currentPage*15+1))
+			if err != nil {
+				results <- core.NewResult("dogpile", nil, err)
+				return
+			}
+
+			scanner := bufio.NewScanner(resp.Body)
+
+			scanner.Split(bufio.ScanWords)
+
+			for scanner.Scan() {
+				for _, str := range domainExtractor.FindAllString(scanner.Text(), -1) {
+					_, found := uniqFilter[str]
+					if !found {
+						uniqFilter[str] = true
+						results <- core.NewResult("dogpile", str, nil)
+					}
+				}
+			}
+
+			resp.Body.Close()
+
+		}
+
+	}(domain, results)
+	return results
+}
