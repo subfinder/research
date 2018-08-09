bddea3c45f167019b12844381db2f98234fb32fb
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-08-09 13:54:04 -0400

    add TestAsk_multi_threaded function

--- core/sources/ask_test.go
+++ core/sources/ask_test.go
@@ -28,3 +28,34 @@ func TestAsk(t *testing.T) {
 	}
 }
 
+func TestAsk_multi_threaded(t *testing.T) {
+	domains := []string{"google.com", "bing.com", "yahoo.com", "duckduckgo.com"}
+	source := Ask{}
+	results := []*core.Result{}
+
+	wg := sync.WaitGroup{}
+	mx := sync.Mutex{}
+
+	for _, domain := range domains {
+		wg.Add(1)
+		go func(domain string) {
+			defer wg.Done()
+			for result := range source.ProcessDomain(domain) {
+				t.Log(result)
+				if result.IsSuccess() && result.IsFailure() {
+					t.Error("got a result that was a success and failure")
+				}
+				mx.Lock()
+				results = append(results, result)
+				mx.Unlock()
+			}
+		}(domain)
+	}
+
+	wg.Wait() // collect results
+
+	if len(results) <= 4 {
+		t.Errorf("expected at least 4 results, got '%v'", len(results))
+	}
+}
+
