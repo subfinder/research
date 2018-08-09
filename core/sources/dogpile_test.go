4613cad4da7c83cd22c0c20b64b9b59e88ac8152
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-08-09 11:27:26 -0400

    add TestDogPile function

--- core/sources/dogpile_test.go
+++ core/sources/dogpile_test.go
@@ -8,3 +8,22 @@ import (
 	"github.com/subfinder/research/core"
 )
 
+func TestDogPile(t *testing.T) {
+	domain := "google.com"
+	source := DogPile{}
+	results := []*core.Result{}
+
+	for result := range source.ProcessDomain(domain) {
+		t.Log(result)
+		results = append(results, result)
+		// Not waiting around to iterate all the possible pages.
+		if len(results) >= 20 {
+			break
+		}
+	}
+
+	if !(len(results) >= 20) {
+		t.Errorf("expected more than 20 result(s), got '%v'", len(results))
+	}
+}
+
