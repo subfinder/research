410e578de9b29147f79df219df356b57d317de29
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-08-12 12:19:58 -0400

    add ProcessDomain method

--- core/sources/threatcrowd.go
+++ core/sources/threatcrowd.go
@@ -10,3 +10,46 @@ import (
 // ThreatCrowd is a source to process subdomains from https://threatcrowd.com
 type ThreatCrowd struct{}
 
+// ProcessDomain takes a given base domain and attempts to enumerate subdomains.
+func (source *ThreatCrowd) ProcessDomain(domain string) <-chan *core.Result {
+	results := make(chan *core.Result)
+	go func(domain string, results chan *core.Result) {
+		defer close(results)
+
+		domainExtractor, err := core.NewSubdomainExtractor(domain)
+		if err != nil {
+			results <- core.NewResult("threatcrowd", nil, err)
+			return
+		}
+
+		uniqFilter := map[string]bool{}
+
+		resp, err := core.HTTPClient.Get("https://www.threatcrowd.org/searchApi/v2/domain/report/?domain=" + domain)
+		if err != nil {
+			results <- core.NewResult("threatcrowd", nil, err)
+			return
+		}
+
+		if resp.StatusCode != 200 {
+			resp.Body.Close()
+			results <- core.NewResult("threatcrowd", nil, errors.New(resp.Status))
+			return
+		}
+
+		scanner := bufio.NewScanner(resp.Body)
+
+		for scanner.Scan() {
+			for _, str := range domainExtractor.FindAllString(scanner.Text(), -1) {
+				_, found := uniqFilter[str]
+				if !found {
+					uniqFilter[str] = true
+					results <- core.NewResult("threatcrowd", str, nil)
+				}
+			}
+		}
+
+		resp.Body.Close()
+
+	}(domain, results)
+	return results
+}
