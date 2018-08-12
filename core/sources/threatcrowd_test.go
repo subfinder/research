1c8dd0bd9f7d35a5bafe3780cf791081727a5144
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-08-12 12:20:24 -0400

    add TestThreatCrowd function

--- core/sources/threatcrowd_test.go
+++ core/sources/threatcrowd_test.go
@@ -8,3 +8,23 @@ import (
 	"github.com/subfinder/research/core"
 )
 
+func TestThreatCrowd(t *testing.T) {
+	domain := "google.com"
+	source := ThreatCrowd{}
+	results := []*core.Result{}
+
+	for result := range source.ProcessDomain(domain) {
+		//t.Log(result)
+		fmt.Println(result)
+		results = append(results, result)
+		// Not waiting around to iterate all the possible pages.
+		if len(results) >= 20 {
+			break
+		}
+	}
+
+	if !(len(results) >= 20) {
+		t.Errorf("expected more than 20 result(s), got '%v'", len(results))
+	}
+}
+
