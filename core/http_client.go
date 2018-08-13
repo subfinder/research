b796ef79db06c9a953e6b139796185799a3600ec
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-08-13 14:33:29 -0400

    add Dialer KeepAlive and MaxIdleConns values

--- core/http_client.go
+++ core/http_client.go
@@ -10,8 +10,11 @@ import (
 var HTTPClient = &http.Client{
 	Transport: &http.Transport{
 		Dial: (&net.Dialer{
-			Timeout: 30 * time.Second,
+			Timeout:   30 * time.Second,
+			KeepAlive: 600 * time.Second,
 		}).Dial,
+		MaxIdleConns:          100,
+		MaxIdleConnsPerHost:   100,
 		TLSHandshakeTimeout:   30 * time.Second,
 		IdleConnTimeout:       30 * time.Second,
 		ResponseHeaderTimeout: 60 * time.Second,
