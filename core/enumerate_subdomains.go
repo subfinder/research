72dbdb25994a5dd95f21fe3f907ef874c602c6b0
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-08-20 10:50:40 -0400

    add lots of inline documentation to the EnumerateSubdomains function

--- core/enumerate_subdomains.go
+++ core/enumerate_subdomains.go
@@ -20,18 +20,37 @@ import (
 //
 //
 func EnumerateSubdomains(domain string, options *EnumerationOptions) <-chan *Result {
+	// this channel of results will be used to combine the result channels
+	// from each source configured in the EnumerationOptions
 	results := make(chan *Result)
+	// the main processing will be done in the background
 	go func() {
-		//defer fmt.Println("*** done sending results ***")
+		// close up the combined results channel when the go parent func returns
 		defer close(results)
+
+		// a wait group to ensure all child go funcs finish processing
 		wg := sync.WaitGroup{}
+
+		// iterate over each source provided in the EnumerationOptions
 		for _, source := range options.Sources {
+
+			// register a job in the wait group
 			wg.Add(1)
+
+			// spawn a go func with the current source in the iteration
 			go func(source Source) {
+
+				// Tell the wait group a job has been completed when the go func returns
 				defer wg.Done()
+
+				// ctx is used for ensuring there are no lingering go funcs to avoid memory leaks
 				ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 				defer cancel()
+
+				// get the results channel from the source calling the ProcessDomain method on it
 				sourceResults := source.ProcessDomain(domain)
+
+				// for loop over results in a select to allow for timeout
 				for {
 					select {
 					case result, ok := <-sourceResults:
@@ -40,15 +59,12 @@ func EnumerateSubdomains(domain string, options *EnumerationOptions) <-chan *Res
 							case results <- result:
 								// no timeout
 							case <-ctx.Done():
-								//fmt.Println("*** time after on pass along ***")
+								// timed out while passing result to combined results channel
 								return
 							}
-							//} else {
-							//	//fmt.Println("*** not ok ***")
-							//	return
 						}
 					case <-ctx.Done():
-						//fmt.Println("*** ctx done ***")
+						// timed out while getting a result from the source's results channel
 						return
 					}
 				}
@@ -56,5 +72,6 @@ func EnumerateSubdomains(domain string, options *EnumerationOptions) <-chan *Res
 		}
 		wg.Wait()
 	}()
+	// this function returns the combined results channel right away
 	return results
 }
