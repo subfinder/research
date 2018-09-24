99397eba51b65b77b993fea63a045aa90f035acb
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-09-24 11:01:53 -0400

    fix subzero to use new context API

--- subzero/main.go
+++ subzero/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/subfinder/research/core"
@@ -27,30 +29,21 @@ var sourcesList = []core.Source{
 	&sources.ThreatCrowd{},
 }
 
-func enumerate(domain string) chan *core.Result {
-	wg := sync.WaitGroup{}
-	results := make(chan *core.Result, len(sourcesList)*4)
-	go func(domain string) {
-		defer close(results)
-		for _, source := range sourcesList {
-			wg.Add(1)
-			go func(domain string, source core.Source, results chan *core.Result) {
-				defer wg.Done()
-				for result := range source.ProcessDomain(domain) {
-					results <- result
-				}
-			}(domain, source, results)
-		}
-		wg.Wait()
-	}(domain)
-	return results
-}
-
 func main() {
 	results := make(chan *core.Result)
 	jobs := sync.WaitGroup{}
 	var cmdEnumerateVerboseOpt bool
 	var cmdEnumerateInsecureOpt bool
+	var cmdEnumerateLimitOpt int
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	opts := &core.EnumerationOptions{
+		Sources: sourcesList,
+		Context: ctx,
+		Cancel:  cancel,
+	}
 
 	var cmdEnumerate = &cobra.Command{
 		Use:   "enumerate [domains to enumerate]",
@@ -67,22 +60,28 @@ func main() {
 				jobs.Add(1)
 				go func(domain string) {
 					defer jobs.Done()
-					for result := range core.EnumerateSubdomains(domain, &core.EnumerationOptions{Sources: sourcesList}) {
+					for result := range core.EnumerateSubdomains(domain, opts) {
 						results <- result
 					}
 				}(domain)
 			}
 		},
 		PostRun: func(cmd *cobra.Command, args []string) {
+			var count = 0
 			for result := range results {
+				count++
 				if result.IsSuccess() {
 					fmt.Println(result.Type, result.Success)
 				} else if cmdEnumerateVerboseOpt {
 					fmt.Println(result.Type, result.Failure)
 				}
+				if cmdEnumerateLimitOpt != 0 && cmdEnumerateLimitOpt == count {
+					cancel()
+				}
 			}
 		},
 	}
+	cmdEnumerate.Flags().IntVar(&cmdEnumerateLimitOpt, "limit", 0, "Limit the reported results to the given number.")
 	cmdEnumerate.Flags().BoolVar(&cmdEnumerateVerboseOpt, "verbose", false, "Show errors and other available diagnostic information.")
 	cmdEnumerate.Flags().BoolVar(&cmdEnumerateInsecureOpt, "insecure", false, "Use potentially insecure sources using http.")
 
