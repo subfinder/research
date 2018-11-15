8e4f4c2146c13201ed3b08962a1c8b2cc0a588b4
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-11-14 20:09:28 -0500

    expand certspotter
    * remove uniq filter, and moves it to test

--- core/sources/certspotter.go
+++ core/sources/certspotter.go
@@ -3,22 +3,33 @@ package sources
 import (
 	"bufio"
 	"context"
+	"encoding/base64"
 	"errors"
 	"net/http"
+	"strings"
+	"sync"
 
 	"github.com/subfinder/research/core"
 )
 
 // CertSpotter is a source to process subdomains from https://certspotter.com
-type CertSpotter struct{}
+type CertSpotter struct {
+	APIToken string
+}
 
 // ProcessDomain takes a given base domain and attempts to enumerate subdomains.
 func (source *CertSpotter) ProcessDomain(ctx context.Context, domain string) <-chan *core.Result {
 	var resultLabel = "certspotter"
 
+	wg := sync.WaitGroup{}
+
+	wg.Add(3)
+
 	results := make(chan *core.Result)
+
+	// apiv0
 	go func(domain string, results chan *core.Result) {
-		defer close(results)
+		defer wg.Done()
 
 		domainExtractor, err := core.NewSubdomainExtractor(domain)
 		if err != nil {
@@ -26,8 +37,10 @@ func (source *CertSpotter) ProcessDomain(ctx context.Context, domain string) <-c
 			return
 		}
 
-		uniqFilter := map[string]bool{}
-		req, err := http.NewRequest(http.MethodGet, "https://certspotter.com/api/v0/certs?domain="+domain, nil)
+		url := "https://certspotter.com/api/v0/certs?domain=" + domain
+
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+
 		if err != nil {
 			sendResultWithContext(ctx, results, core.NewResult(resultLabel, nil, err))
 			return
@@ -54,12 +67,8 @@ func (source *CertSpotter) ProcessDomain(ctx context.Context, domain string) <-c
 				return
 			}
 			for _, str := range domainExtractor.FindAllString(scanner.Text(), -1) {
-				_, found := uniqFilter[str]
-				if !found {
-					uniqFilter[str] = true
-					if !sendResultWithContext(ctx, results, core.NewResult(resultLabel, str, nil)) {
-						return
-					}
+				if !sendResultWithContext(ctx, results, core.NewResult(resultLabel, str, nil)) {
+					return
 				}
 			}
 		}
@@ -72,5 +81,146 @@ func (source *CertSpotter) ProcessDomain(ctx context.Context, domain string) <-c
 		}
 
 	}(domain, results)
+
+	// apiv1 certs
+	go func(domain string, results chan *core.Result) {
+		defer wg.Done()
+
+		domainExtractor, err := core.NewSubdomainExtractor(domain)
+		if err != nil {
+			sendResultWithContext(ctx, results, core.NewResult(resultLabel, nil, err))
+			return
+		}
+
+		url := "https://api.certspotter.com/v1/certs?domain=" + domain + "&include_subdomains=true&expand=dns_names&match_wildcards=true"
+
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+
+		if err != nil {
+			sendResultWithContext(ctx, results, core.NewResult(resultLabel, nil, err))
+			return
+		}
+
+		if source.APIToken != "" {
+			req.Header.Set("Authorization", "Bearer "+source.APIToken)
+		}
+
+		req.WithContext(ctx)
+
+		resp, err := core.HTTPClient.Do(req)
+		if err != nil {
+			sendResultWithContext(ctx, results, core.NewResult(resultLabel, nil, err))
+			return
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != 200 {
+			sendResultWithContext(ctx, results, core.NewResult(resultLabel, nil, errors.New(resp.Status)))
+			return
+		}
+
+		scanner := bufio.NewScanner(resp.Body)
+
+		for scanner.Scan() {
+			if ctx.Err() != nil {
+				return
+			}
+
+			if !strings.Contains(scanner.Text(), `"data":"`) {
+				continue
+			}
+
+			str := scanner.Text()
+			str = strings.TrimSpace(str)
+			str = strings.Trim(str, `"data":"`)
+			str = strings.Trim(str, `"`)
+
+			decodedData, err := base64.StdEncoding.DecodeString(str)
+
+			if err != nil {
+				continue
+			}
+
+			for _, str := range domainExtractor.FindAllString(string(decodedData), -1) {
+				if !sendResultWithContext(ctx, results, core.NewResult(resultLabel, str, nil)) {
+					return
+				}
+			}
+		}
+
+		err = scanner.Err()
+
+		if err != nil {
+			sendResultWithContext(ctx, results, core.NewResult(resultLabel, nil, err))
+			return
+		}
+
+	}(domain, results)
+
+	// apiv1 issuances
+	go func(domain string, results chan *core.Result) {
+		defer wg.Done()
+
+		domainExtractor, err := core.NewSubdomainExtractor(domain)
+		if err != nil {
+			sendResultWithContext(ctx, results, core.NewResult(resultLabel, nil, err))
+			return
+		}
+
+		url := "https://api.certspotter.com/v1/issuances?domain=" + domain + "&include_subdomains=true&expand=dns_names&match_wildcards=true"
+
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+
+		if err != nil {
+			sendResultWithContext(ctx, results, core.NewResult(resultLabel, nil, err))
+			return
+		}
+
+		if source.APIToken != "" {
+			req.Header.Set("Authorization", "Bearer "+source.APIToken)
+		}
+
+		req.WithContext(ctx)
+
+		resp, err := core.HTTPClient.Do(req)
+		if err != nil {
+			sendResultWithContext(ctx, results, core.NewResult(resultLabel, nil, err))
+			return
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != 200 {
+			sendResultWithContext(ctx, results, core.NewResult(resultLabel, nil, errors.New(resp.Status)))
+			return
+		}
+
+		scanner := bufio.NewScanner(resp.Body)
+
+		for scanner.Scan() {
+			if ctx.Err() != nil {
+				return
+			}
+			for _, str := range domainExtractor.FindAllString(scanner.Text(), -1) {
+				if !sendResultWithContext(ctx, results, core.NewResult(resultLabel, str, nil)) {
+					return
+				}
+			}
+		}
+
+		err = scanner.Err()
+
+		if err != nil {
+			sendResultWithContext(ctx, results, core.NewResult(resultLabel, nil, err))
+			return
+		}
+
+	}(domain, results)
+
+	go func() {
+		defer close(results)
+
+		wg.Wait()
+	}()
+
 	return results
 }
