c5b5d238eedd5be4477e9a13286ada6b330dc31b
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-11-25 14:06:33 -0500

    Update subdomain_extractor.go

--- core/subdomain_extractor.go
+++ core/subdomain_extractor.go
@@ -1,10 +1,17 @@
 package core
 
-import "regexp"
+import (
+	"regexp"
+	"sync"
+)
+
+var subdomainExtractorMutex = &sync.Mutex{}
 
 // NewSubdomainExtractor creates a new regular expression to extract
 // subdomains from text based on the given domain.
 func NewSubdomainExtractor(domain string) (*regexp.Regexp, error) {
+	subdomainExtractorMutex.Lock()
+	defer subdomainExtractorMutex.Unlock()
 	extractor, err := regexp.Compile(`[a-zA-Z0-9\*_.-]+\.` + domain)
 	if err != nil {
 		return nil, err
