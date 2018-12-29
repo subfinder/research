1e34569d2dfd70bd8422b4f979f3a231c34ba566
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-12-29 17:54:59 -0500

    Update http_client.go

--- core/http_client.go
+++ core/http_client.go
@@ -3,18 +3,32 @@ package core
 import (
 	"net"
 	"net/http"
+	"strings"
 	"time"
+
+	"github.com/viki-org/dnscache"
 )
+
+var dnsCache = dnscache.New(5 * time.Minute)
 
 // HTTPClient is a reusable component that can be used in sources.
 var HTTPClient = &http.Client{
 	Transport: &http.Transport{
-		Dial: (&net.Dialer{
-			Timeout:   15 * time.Second,
-			KeepAlive: 60 * time.Second,
-		}).Dial,
-		MaxIdleConns:        100,
-		MaxIdleConnsPerHost: 2,
+		MaxIdleConnsPerHost: 64,
+		Dial: func(network string, address string) (net.Conn, error) {
+			separator := strings.LastIndex(address, ":")
+			ip, err := dnsCache.FetchOneString(address[:separator])
+			if err != nil {
+				return nil, err
+			}
+			return net.Dial("tcp", ip+address[separator:])
+		},
+		//Dial: (&net.Dialer{
+		//	Timeout:   15 * time.Second,
+		//	KeepAlive: 60 * time.Second,
+		//}).Dial,
+		MaxIdleConns: 100,
+		//MaxIdleConnsPerHost: 2,
 		//MaxConnsPerHost:       1,
 		TLSHandshakeTimeout:   10 * time.Second,
 		IdleConnTimeout:       10 * time.Second,
