3f3a963c3a0d95c6a113b32caeaf712eddb15efd
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-12-29 16:49:13 -0500

    Update http_client.go

--- core/http_client.go
+++ core/http_client.go
@@ -10,14 +10,30 @@ import (
 var HTTPClient = &http.Client{
 	Transport: &http.Transport{
 		Dial: (&net.Dialer{
-			Timeout:   30 * time.Second,
-			KeepAlive: 600 * time.Second,
+			Timeout: 15 * time.Second,
+			//KeepAlive: 600 * time.Second,
 		}).Dial,
-		MaxIdleConns:          100,
-		MaxIdleConnsPerHost:   100,
-		TLSHandshakeTimeout:   30 * time.Second,
-		IdleConnTimeout:       30 * time.Second,
-		ResponseHeaderTimeout: 60 * time.Second,
-		ExpectContinueTimeout: 30 * time.Second,
+		MaxIdleConns:          200,
+		MaxIdleConnsPerHost:   1,
+		MaxConnsPerHost:       1,
+		TLSHandshakeTimeout:   10 * time.Second,
+		IdleConnTimeout:       10 * time.Second,
+		ResponseHeaderTimeout: 10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
 	},
 }
+
+// var HTTPClient = &http.Client{
+//	Transport: &http.Transport{
+//		Dial: (&net.Dialer{
+//			Timeout:   30 * time.Second,
+//			KeepAlive: 600 * time.Second,
+//		}).Dial,
+//		MaxIdleConns:          100,
+//		MaxIdleConnsPerHost:   100,
+//		TLSHandshakeTimeout:   30 * time.Second,
+//		IdleConnTimeout:       30 * time.Second,
+//		ResponseHeaderTimeout: 60 * time.Second,
+//		ExpectContinueTimeout: 30 * time.Second,
+//	},
+//}
